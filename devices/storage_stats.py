e100a899679e3a898ed929c54253fca212ef2d91
Fraser Speirs <fs@cedars.inverclyde.sch.uk>
2016-07-20 16:20:46 +0100

    Improved progress on storage stats script

--- devices/storage_stats.py
+++ devices/storage_stats.py
@@ -1,8 +1,8 @@
 #!/bin/python
 
 import sys
-sys.path.insert(0, '../lib')
 import JSSLib
+from time import sleep
 
 # Get all devices in JSS
 devices = JSSLib.get_mobile_devices()
@@ -11,18 +11,19 @@ stats = {}
 
 print 'Getting storage stats for %s devices.' % len(devices)
 
+progress=0.0
+each_device = len(devices)/100.0
+
 for d in devices:
+	sys.stdout.write('\r')
 	percent_used = JSSLib.get_device_storage_used_percentage(d['id'])
 	user_name = JSSLib.get_device_assigned_user(d['id'])
-
+	
 	if user_name == '':
 		continue
 	
 	user_id = JSSLib.get_user_by_username(user_name)['id']
 	graduation_year = JSSLib.get_user_extension_attributes(user_id)['Graduation Year']
-	
-	# Show progress
-	print '.'
 	
 	if not graduation_year in stats.keys():
 		stats[graduation_year] = (0,0)
@@ -32,6 +33,10 @@ for d in devices:
 	
 	stats[graduation_year] = (percent, denominator)
 
+	progress += each_device
+	sys.stdout.write("Device %s - %d%%" % (d['id'], progress))
+	sys.stdout.flush()
+	sleep(0.25)
 
 # Compute stats
 overall_percent = 0
