a26da39a163b9f70278ec713f9aebe5b7d9aa98a
Fraser Speirs <fs@cedars.inverclyde.sch.uk>
2016-07-21 09:33:21 +0100

    Tidied up inventory_mobileapps

--- apps/inventory_mobileapps.py
+++ apps/inventory_mobileapps.py
@@ -1,67 +1,6 @@
-##
-# This is used to update device names to be the same as the username assigned to the device
-#
-# You can run this by typing the following:
-#       python update_devicename.py
-#
-# If you have any issues, please submit an issue on this GitHub Repo and
-# I can try to help you at the next chance I get.
-#
-# Set the URL, Username and Password where stated below.
-# They currently have placeholders in there, just replace them with your information
-#
-# For this script, the user you enter must have the following permissions:
-#   - Mobile Devices
-#       - Read
-#       - Write
-##
+import JSSLib
 
-import requests, sys, json, urllib
-
-# Set this to False if you have any errors
-verifySSL = False
-
-# Set this so it only goes through one device for testing
-debug = True
-debug_username = 'ls'
-
-# Set JSS Variables here
-# JSS URL must end with / like so:
-# https://jss.example.com:8443/
-jssURL = ''
-jssAPIUsername = ''
-jssAPIPassword = ''
-
-# I have my own JSS Module for easy code publishing
-try:
-    from vcpmodule import credentials as jssCred
-    credentials_loaded = jssCred.isLoaded()
-    creds = jssCred.getJSS()
-    jssURL = creds.url
-    jssAPIUsername = creds.username
-    jssAPIPassword = creds.password
-except:
-    credentials_loaded = False
-
-##                              ##
-# DO NOT CHANGE ANY OF THE BELOW #
-##                              ##
-
-error_reached = False
-
-jssAPIURL = jssURL + "JSSResource"
-
-requestHeaders = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-}
-
-try:
-    result = json.loads(requests.get('%s/mobiledeviceapplications' % jssAPIURL, headers=requestHeaders, verify=verifySSL, auth=(jssAPIUsername, jssAPIPassword)).text)
-    apps = result['mobile_device_applications']
-except Exception as e:
-    print(e)
-    sys.exit(0)
+apps = JSSLib.get_apps()
 
 # Simply loop through all the records in the JSS
 for app in apps:
@@ -74,10 +13,5 @@ for app in apps:
         print '\"%s\",%s' % (app_name, app_version)
 
     except Exception as e:
-        error_reached = True
         print(e)
 
-if error_reached:
-    print('Error renaming devices')
-else:
-    print('Finished renaming devices')
